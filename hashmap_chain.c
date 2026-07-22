4b54e84e4d9dbacf95bf9f04c957232781f9e6f8
Fredyprez <fredypz116@gmail.com>
2026-07-22 01:38:35 -0400

    created prototype initOpenHashMap()

--- hashmap_chain.c
+++ hashmap_chain.c
@@ -1,6 +1,6 @@
 #include "hashmap.h"
 #include "hashmap_chain.h"
 
-// void insertStudentChain(struct HashMap* hashTable, int studentKey, char* studentName, char* studentEmail){
+void initOpenHashMap(struct HashMap* hashMapOpen){
     
-// }
+}
